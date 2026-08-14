a690c697282492aea0b9749d566b13bb6e46de7a
Adam Snyder <adam.snyder@noirlab.edu>
2026-08-13 17:12:52 -0700

    Updated KHT detector to optionally allow for binning; no further upgrades made.

--- src/astro_lfd/algorithms/__init__.py
+++ src/astro_lfd/algorithms/__init__.py
@@ -1,4 +1,3 @@
 from .khtDetect import KHTDetectConfig, KHTDetectTask
-from .shtDetect import SHTDetectConfig, SHTDetectTask
 
-__all__ = ["KHTDetectConfig", "KHTDetectTask", "SHTDetectConfig", "SHTDetectTask"]
+__all__ = ["KHTDetectConfig", "KHTDetectTask"]
